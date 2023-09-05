7121f03fd4f5b10611e8ab3083787d7683fee7f4
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-05 11:42:16 -0300

    feat: :rocket: setup initial project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express, { Response } from "express";
+import "express-async-errors";
+import { errorHandling } from "./utils/error";
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "5mb" }));
+
+app.use("/health", (_, response: Response) => {
+  return response.status(200).json({ status: "ok" });
+});
+
+app.use(errorHandling);
---
+++ src/utils/error.ts
@@ -0,0 +1,24 @@
+import { env } from "@/env";
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+
+export function errorHandling(
+  error: Error,
+  _: Request,
+  response: Response,
+  __: NextFunction
+) {
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .send({ message: "Validation error", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Add some external analytics tool
+  }
+
+  return response.status(500).send({ message: "Internal server error" });
+}
