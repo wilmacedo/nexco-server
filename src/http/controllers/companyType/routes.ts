40ea88c0b82cbeee40b8f8fd1d926a4088a11e4c
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-08 11:59:10 -0300

    feat: :sparkles: add auth middleware to another routes

--- src/http/controllers/companyType/routes.ts
+++ src/http/controllers/companyType/routes.ts
@@ -1,8 +1,9 @@
+import { verifyJwt } from "@/http/middlewares/verifyJwt";
 import { Router } from "express";
 import { list } from "./list";
 import { register } from "./register";
 
 export const companyTypeRouter = Router();
 
-companyTypeRouter.post("/company-types", register);
-companyTypeRouter.get("/company-types", list);
+companyTypeRouter.post("/company-types", verifyJwt, register);
+companyTypeRouter.get("/company-types", verifyJwt, list);
--- src/http/controllers/interests/routes.ts
+++ src/http/controllers/interests/routes.ts
@@ -1,8 +1,9 @@
+import { verifyJwt } from "@/http/middlewares/verifyJwt";
 import { Router } from "express";
 import { list } from "./list";
 import { register } from "./register";
 
 export const interestsRouter = Router();
 
-interestsRouter.post("/interests", register);
-interestsRouter.get("/interests", list);
+interestsRouter.post("/interests", verifyJwt, register);
+interestsRouter.get("/interests", verifyJwt, list);
